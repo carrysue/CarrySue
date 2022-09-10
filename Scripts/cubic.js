0bcf2706e71bad739aebb3d56a0d825e3bdfbaf6
carrysue <108021172+carrysue@users.noreply.github.com>
2022-09-10 16:54:52 +0800

    Update cubic.js

--- Scripts/cubic.js
+++ Scripts/cubic.js
@@ -1,1 +1,35 @@
-qqq
+/******************************
+
+脚本功能：cubox解锁
+脚本作者：CarrySue
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+
+*******************************
+
+[rewrite_local]
+# > Cubox 解锁高级版订阅
+^https:\/\/cubox\.pro\/c\/api\/.+ url script-response-body cubox.js
+[mitm] 
+hostname = cubox.pro
+
+*******************************/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/userInfo';
+const pay = '/userPay';
+
+if (url.indexOf(vip) != -1) {
+    obj.data.level = 2;
+    obj.data.isExpire = false;
+    obj.data.expireTime = "2209-12-31T23:59:59+08:00";
+    body = JSON.stringify(obj);
+}
+if (url.indexOf(pay) != -1) {
+    obj.data.level = 2;
+    obj.data.isExpire = false;
+    obj.data.expireTime = "2209-12-31T23:59:59+08:00";
+$done({body});
